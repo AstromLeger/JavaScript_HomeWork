76e2ad23a96712bff011e41199ab49c53d100f97
Aleksei Vaskov <prizrak1143@gmail.com>
2023-03-14 17:26:49 +0300

    домашнее задание 1 и 2

---
+++ Home_Work/Home_Work_1/script.js
@@ -0,0 +1,10 @@
+// // Задание 1
+// let Fahrenheit = 0;
+// const DegreesCelsius = 125;
+// Fahrenheit = (9/5) * DegreesCelsius + 32;
+// console.log(` Градусы Цельсия = ${DegreesCelsius} Градусы Фаренгейта = ${Fahrenheit}`);
+
+// // Задание 2
+// let Name = "Лёха";
+// const Admin = Name;
+// console.log(Admin)
---
+++ Home_Work/Home_Work_2/script.js
@@ -0,0 +1,55 @@
+// // Задание 1
+// let num1 = +prompt('Введите первое число)')
+// let num2 = +prompt('Введите второе число))')
+
+// if (num1<=1) {
+//   console.log(`num1 меньше или равена 1`)
+// } else {
+//   console.log(`num1 больше 1`)
+// }
+
+// if (num2>=3) {
+//   console.log(`num1 больше или равена 3`)
+// } else {
+//   console.log(`num1 меньше 3`)
+// }
+
+// // Задание 2
+// let test = true;
+// if (test === true) {
+// console.log('+++');
+// } else {
+// console.log('---');
+// }
+
+// Задание 3
+// opredelenie_Dekat();
+
+// function opredelenie_Dekat() {
+//   let day = +prompt('Введите число от 1 до 31, пожалуйста!')
+
+//   if (day>= 1 && day <=10) {
+//     console.log(`Этот день относится к первой декате`)
+//   } else if (day>= 11 && day <=20) {
+//     console.log(`Этот день относится к второй декате`)
+//   }
+//   else if (day>= 21 && day <=31){
+//     console.log(`Этот день относится к третьей декате`)
+//   }
+//   else {
+//     console.log(`На всякий случай я подстраховался, на тот случай если вы введёте неверное число)`)
+//     alert ('Я преполагал что вы введёте неверное число, поэтому прошу, введите правильное число')
+//     opredelenie_Dekat ()
+//   }
+// }
+
+
+
+// // Задание 4
+// let number = +prompt('Введите число, пожалуйста')
+// let hundreds = Math.trunc(number/100) % 10
+// let dozens = Math.trunc(number/10) % 10
+// let units = number % 10
+// console.log(`Количество сотен - ${hundreds}, Количество десятков - ${dozens}, Количество единиц - ${units}`)
+
+
