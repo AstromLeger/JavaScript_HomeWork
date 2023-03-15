963d59ca2a55981a02d1bdadcb11dc704a7efb89
Aleksei Vaskov <prizrak1143@gmail.com>
2023-03-15 17:53:13 +0300

    домашнее задание 3

---
+++ Home_Work/Home_Work_3/script.js
@@ -0,0 +1,131 @@
+// // Задание 1
+// let firstNumber = +prompt(`Введите первое число`)
+// let secondNumber = +prompt(`Введите второе число`)
+// console.log(`Куб ${firstNumber} = ${firstNumber**3}`)
+// console.log(`Куб ${secondNumber} = ${Math.pow(secondNumber, 3)}`)
+// console.log(`Сумма кубов ${firstNumber} и ${secondNumber} = ${firstNumber**3 + Math.pow(secondNumber, 3)}`)
+
+// // Задание 2
+// RaschetZarplatu()
+// function RaschetZarplatu(){
+// let Zarplata = +prompt(`Введите число`)
+// if (isNaN(Zarplata)) {
+//   alert(`Вы ввели не число`)
+//   RaschetZarplatu()
+// } else {
+//   Zarplata = Zarplata - (Zarplata / 100 * 13)
+//   console.log(`Расчет заработной платы за вычетом налогов равен ${Zarplata}`)
+// }
+// }
+
+// // Задание 3
+// let firstNumber = +prompt(`Введите первое число`)
+// let secondNumber = +prompt(`Введите второе число`)
+// let thirdNumber = +prompt(`Введите третье число`)
+// let max = 0
+// if (firstNumber>secondNumber) {
+//   max = firstNumber
+// } else {
+//   max = secondNumber
+// }
+// if (max<thirdNumber) {
+// max = thirdNumber
+// } 
+// console.log(`Первое число ${firstNumber}`)
+// console.log(`Второе число ${secondNumber}`)
+// console.log(`Третье число ${thirdNumber}`)
+// console.log(`Mаксимальное число ${max}`)
+
+// Задание 4
+let firstNumber = +prompt(`Введите первое число`)
+let secondNumber = +prompt(`Введите второе число`)
+console.log(`Сумма чисел ${firstNumber} и ${secondNumber} = ${Sum(firstNumber, secondNumber)}`)
+console.log(`Разность чисел ${firstNumber} и ${secondNumber} = ${Raznost(firstNumber, secondNumber)}`)
+console.log(`Произведение чисел ${firstNumber} и ${secondNumber} = ${Proizvedenie(firstNumber, secondNumber)} `)
+console.log(`Частное чисел ${firstNumber} и ${secondNumber} = ${Chastnoe(firstNumber, secondNumber)} `)
+function Sum(num1, num2) {
+  return num1 + num2
+}
+function Raznost(num1, num2) {
+  let min = 0
+  let max = 0
+  if (num1 > num2) {
+    min = num2
+    max = num1
+  } else if (num1 < num2){
+    min = num1
+    max = num2
+  }
+  else {
+    console.log(`0 так как числа равны`)
+  }
+  return max - min
+
+  // Можно было проще - результату разности присвоить умножение на -1
+}
+function Proizvedenie(num1, num2) {
+  return num1 * num2
+}
+function Chastnoe(num1, num2) {
+  return num1 / num2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
